bb784d659d784a131a375029e3840e031f2cb07a
Didierchavarria <dchavarriab@vlacademy.com>
2025-02-03 21:25:58 -0600

    js calculos

--- Proyecto_Final_2025/JS/PagesJS/Practica_Calculadora.js
+++ Proyecto_Final_2025/JS/PagesJS/Practica_Calculadora.js
@@ -9,19 +9,26 @@ console.log(`Funcionando Archivo: Practica_Calculadora`);
 //Alert, console.log / Inner
  console.log('Mostrar nombre:',nombre);
  console.log('Mostrar edad:',edad);
-
+ alert(`Nombre ${nombre} edad ${edad}`)
 }
 
 
-function realizarSuma () {
-    console.log("funcionando realizarSuma");
 
-    let numero1 =document.getElementById('numero1').value;
-    let numero2 =document.getElementById('numero2').value;
 
+function realizarSuma() {
+    console.log("Funcionando realizarSuma")
+ 
+    let numero1 = document.getElementById('numero1').value;//gi
+    let numero2 = document.getElementById('numero2').value;
+ 
+    //LS , LO , LA
+    //Typescript int decimal
+    //ParseInt 
+    let Suma = parseInt(numero1)  + parseInt(numero2) ;
+ 
+    document.getElementById('resultado').innerText = Suma;
+ 
     console.log(numero1)
-    console.log(numero2);
-
-
+    console.log(numero2)
+    console.log(Suma)
 }
-
--- Proyecto_Final_2025/JS/PagesJS/Practica_IF.js
+++ Proyecto_Final_2025/JS/PagesJS/Practica_IF.js
@@ -1,1 +1,63 @@
 console.log(`Funcionando Archivo: Practica_IF`);
+
+//caso 1 
+function verificarEdad () {
+    
+
+    let edad = document.getElementById('edad').value;//gi
+    let pais = document.getElementById('pais').value;
+
+    console.log(edad)
+    console.log(pais)
+
+    if (edad >= 18) {
+        alert(` Eres mayor de edad en ${pais}`)
+    } else {
+        alert(` Eres menor de edad en ${pais}`)
+    }
+}
+
+//caso 2
+
+function verificarParImpar() {
+    let numero3 = document.getElementById(`numero3`).value
+    let numeroconvertido = parseInt (numero3)
+
+    if (numeroconvertido % 2 === 0) {
+
+        document.ElementById('mensajePar').innerTex = `El numero ${convertido} es par`;
+    } else {
+        document.getElementById('mensajeParImpar').innerTex = `El numero ${convertido} impar`;
+    }
+
+    console.log(numeroconvertido)
+}
+
+function evaluarNota() {
+    
+    let nota = document.getElementById('nota').value;
+
+    let numeroconvertido = parseInt(nota)
+    console.log(numeroconvertido)
+
+    //mayor de 90 excelente nota
+    //mayor a 70 aprobado 
+    //menor 70 reprobado 
+    if (numeroconvertido >= 90) {
+       // alert("excelente nota")
+        document.getElementById('mensajeNota').innerText = `El resultado es excelente nota`;
+    }
+    else if(numeroconvertido >= 70){
+       // alert("aprobado")
+        document.getElementById('mensajeNota').innerText = `El resultado es aprobado`;
+    }     
+    else {
+        //alert("reprobado")
+        document.getElementById('mensajeNota').innerText = `El resultado es reprobado`;
+    }
+}
+
+//parse int
+//parseFloat decimales
+//monto * 0.9
+
