73178cadf8edc3aea5068ec9e79871a49a8f5442
Didierchavarria <dchavarriab@vlacademy.com>
2025-02-03 20:12:08 -0600

    inicio

--- Proyecto_Final_2025/JS/PagesJS/Practica_Calculadora.js
+++ Proyecto_Final_2025/JS/PagesJS/Practica_Calculadora.js
@@ -5,5 +5,28 @@ console.log(`Funcionando Archivo: Practica_Calculadora`);
     let nombre = document.getElementById("nombre").value;
     let edad = document.getElementById("edad").value;
 
+//Mostrar
+//Alert, console.log / Inner
+
+
+ console.log('Mostrar nombre:',nombre);
+ console.log('Mostrar edad:',edad);
+
+
+
+
+}
+
+
+function realizarSuma () {
+    console.log("funcionando realizarSuma");
+
+    let numero1 =document.getElementById('numero1').value;
+    let numero2 =document.getElementById('numero2').value;
+
+    console.log(numero1)
+    console.log(numero2);
+
+
 }
 
