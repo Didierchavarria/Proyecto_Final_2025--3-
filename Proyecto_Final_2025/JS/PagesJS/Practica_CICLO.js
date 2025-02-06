f42e6406deec9666e76b5fd47bbf98abecfe31f3
Didierchavarria <dchavarriab@vlacademy.com>
2025-02-05 19:48:41 -0600

    ejercicio forEach

--- Proyecto_Final_2025/JS/PagesJS/Practica_CICLO.js
+++ Proyecto_Final_2025/JS/PagesJS/Practica_CICLO.js
@@ -6,4 +6,27 @@ let personas = [
     { nombre: "Laura", edad: 16, activo: true },
     { nombre: "Pedro", edad: 40, activo: false }
 ];
+let clientes = [
+    { nombre: "Juan", edad: 25, activo: true },
+    { nombre: "Ana", edad: 17, activo: false },
+    { nombre: "Carlos", edad: 30, activo: true },
+    { nombre: "Laura", edad: 16, activo: true },
+    { nombre: "Pedro", edad: 40, activo: false }
+];
 
+clientes.forEach(element => {
+    console.log(element.nombre,element.activo);
+
+});
+//objeto .
+
+//bucles o ciclos
+
+//doreach
+
+//array quitelo y coloque su array :)
+//element serian las pocisiones 0,1,2
+personas.forEach(element => {
+    console.log(element);
+
+});
