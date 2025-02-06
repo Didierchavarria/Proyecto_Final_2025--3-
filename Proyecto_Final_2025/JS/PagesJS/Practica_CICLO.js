7ad2d7d0691f8f04c99592e07575d6d39ae0c450
Didierchavarria <dchavarriab@vlacademy.com>
2025-02-05 21:29:39 -0600

    5/2 Practica_CICLO

--- Proyecto_Final_2025/JS/PagesJS/Practica_CICLO.js
+++ Proyecto_Final_2025/JS/PagesJS/Practica_CICLO.js
@@ -29,4 +29,91 @@ clientes.forEach(element => {
 personas.forEach(element => {
     console.log(element);
 
+    if (element.activo === true) {
+        console.log(element.nombre,element.activo);
+    }
+    
 });
+
+//pagina 15 y 16 leer 
+
+//cantidad de elementos de un array
+
+console.log(personas.length);
+
+
+//filtrar una propiedad
+let buscarnombre = personas.find(x => x.nombre === "Ana")
+console.log(buscarnombre);
+
+let buscaractivo = personas.find(x => x.activo === true)
+console.log(buscaractivo);
+
+function filtrar() {
+    let valor = document.getElementById('sensitivo').value;
+console.log(valor);
+}
+
+
+
+
+
+
+
+
+
+
+
+//caso 1
+function buscarDesdeInput() {
+    let input1 = document.getElementById('buscarNombre').value;  //Metio el valor pepe
+    let resultado = document.getElementById('resultadoBusqueda');
+  
+    //Busca en el arreglo 
+    let busqueda = personas.find(x=> x.nombre.toUpperCase() === input1.toUpperCase()) //nombre == pepe
+  
+  
+    //Aqui le indicamos si el resultado es verdadero o falso 
+    if (busqueda) {
+      
+      resultado.innerHTML = `encontrado a la persona ${busqueda.nombre}`
+  
+    } else {
+      resultado.innerHTML = `No encontrado a la persona`
+    }
+  }
+
+  //crealr la funcion del onclick
+  //input id value
+  //cualquier etiqueta mostrar el resultado inner
+//caso 2
+  function primeraPersonaActiva() {
+    let resultado= document.getElementById('resultadoActivo'); //inner
+    let busqueda= personas.find(x=> x.activo == true)
+    if (busqueda) {
+        resultado.innerHTML = `primera persona encontrada ${busqueda.nombre}`
+    } else {
+        resultado.innerHTML = `No se encontro nadie activo`
+    }
+  }
+
+//caso 6
+// cuando son muchos es foreach
+function personasActivas() {
+    let resultado = document.getElementById('listaActivos');
+
+    personas.forEach(element => {
+        
+        if (element.activo == true) {
+            
+          resultado.innerHTML += `
+          
+            <li>nombre : ${element.nombre} </li>`
+          
+            
+        }
+
+    });
+}
+//3,4,5
+// entrgable 2 proyecto
