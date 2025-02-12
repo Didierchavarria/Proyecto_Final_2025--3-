670a775c6de76d0d4293f238acbcbb595126db14
Didierchavarria <dchavarriab@vlacademy.com>
2025-02-12 17:42:34 -0600

    entregale 5

--- Proyecto_Final_2025/JS/PagesJS/API_Listado_Clientes.js
+++ Proyecto_Final_2025/JS/PagesJS/API_Listado_Clientes.js
@@ -11,7 +11,7 @@ const Api2 = "https://rickandmortyapi.com/api/character"
 let DatosFiltrados = []
 let DatosFiltrados2 = []
 let contenedor = document.getElementById('tbody');
-
+let contenedor2 = document.getElementById('tbody2'); //Crean contenido 
 
 //aela Visualizar al ejecutar la pagina DOM
 document.addEventListener('DOMContentLoaded', () => {
@@ -50,7 +50,8 @@ function consultarDatos2() {
         .then(result => {           //3- Resultado  result
             // custom error
             console.log(result.results)
-        
+            DatosFiltrados2 = result.results
+            Mostrar_CampoTabla2()
         })
         .catch(error => {      ///4- Catch
             // common error
@@ -85,5 +86,32 @@ function Mostrar_CampoTabla() {
     });
 
     contenedor.innerHTML = html
+
+}
+
+
+function Mostrar_CampoTabla2() {
+    
+    let html = ""
+    
+    DatosFiltrados2.forEach(element => {
+        
+        console.log(element)
+
+        html += `
+        
+         <tr>
+
+            <td> ${element.name} </td>
+            <td> <img src="${element.image}" alt="">  </td>
+        
+        </tr>
+        
+        
+        `
+
+    });
+
+    contenedor2.innerHTML = html
 
 }
